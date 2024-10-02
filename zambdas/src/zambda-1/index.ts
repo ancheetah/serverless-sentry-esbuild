c14f0cb3dccf221826005044286fd8ae8c3a6081
AJ Ancheta <7781450+ancheetah@users.noreply.github.com>
2024-10-02 17:42:50 -0400

    configure ts, eslint, prettier

---
+++ zambdas/src/zambda-1/index.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { wrapHandler } from '@sentry/aws-serverless';
+
+export const index = wrapHandler(async (input: any): Promise<APIGatewayProxyResult> => {
+  try {
+    console.log('zambda-1', input);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'zambda-1 suceeded',
+      }),
+    };
+  } catch (error: any) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'zambda-1 failed',
+      }),
+    };
+  }
+});
